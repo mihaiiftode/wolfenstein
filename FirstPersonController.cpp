0c999f665ba42df42a9dfa15ee4143b634e2970b
Rares Popa <Rares_Popa_2008@Yahoo.com>
2017-10-11 19:00:11 +0200

    * Optimized block translation * Loading map from JSON pt.1

--- FirstPersonController.cpp
+++ FirstPersonController.cpp
@@ -11,77 +11,77 @@ using namespace glm;
 
 
 FirstPersonController::FirstPersonController(sre::Camera* camera)
-	: camera(camera)
+    : camera(camera)
 {
-	camera->setPerspectiveProjection(45, 0.1f, 1000);
+    camera->setPerspectiveProjection(45, 0.1f, 1000);
 }
 
 
 void FirstPersonController::update(float deltaTime)
 {
-	vec3 direction(cos(radians(rotation)), 0, sin(radians(rotation)));
-	rightVector = vec3(cos(radians(rotation + 90)), 0, sin(radians(rotation + 90)));
+    vec3 direction(cos(radians(rotation)), 0, sin(radians(rotation)));
+    rightVector = vec3(cos(radians(rotation+90)), 0, sin(radians(rotation+90)));
 
-	if (forward)
-		position += direction * deltaTime * speed;
-	if (back)
-		position -= direction * deltaTime * speed;
-	if (left)
-		position -= rightVector * deltaTime * speed;
-	if (right)
-		position += rightVector * deltaTime * speed;
+    if (forward)
+        position += direction * deltaTime * speed;
+    if (back)
+        position -= direction * deltaTime * speed;
+    if (left)
+        position -= rightVector * deltaTime * speed;
+    if (right)
+        position += rightVector * deltaTime * speed;
 
-	auto view = lookAt(position, position + direction, {0, 1, 0});
-	camera->setViewTransform(view);
+    auto view = lookAt(position, position+direction, {0, 1, 0});
+    camera->setViewTransform(view);
 }
 
 void FirstPersonController::onKey(SDL_Event& event)
 {
-	if (event.key.keysym.sym == SDLK_w)
-	{
-		if (event.type == SDL_KEYDOWN)
-			forward = true;
-		else if (event.type == SDL_KEYUP)
-			forward = false;
-	}
-	if (event.key.keysym.sym == SDLK_s)
-	{
-		if (event.type == SDL_KEYDOWN)
-			back = true;
-		else if (event.type == SDL_KEYUP)
-			back = false;
-	}
-	if (event.key.keysym.sym == SDLK_a)
-	{
-		if (event.type == SDL_KEYDOWN)
-			left = true;
-		else if (event.type == SDL_KEYUP)
-			left = false;
-	}
-	if (event.key.keysym.sym == SDLK_d)
-	{
-		if (event.type == SDL_KEYDOWN)
-			right = true;
-		else if (event.type == SDL_KEYUP)
-			right = false;
-	}
+    if (event.key.keysym.sym==SDLK_w)
+    {
+        if (event.type==SDL_KEYDOWN)
+            forward = true;
+        else if (event.type==SDL_KEYUP)
+            forward = false;
+    }
+    if (event.key.keysym.sym==SDLK_s)
+    {
+        if (event.type==SDL_KEYDOWN)
+            back = true;
+        else if (event.type==SDL_KEYUP)
+            back = false;
+    }
+    if (event.key.keysym.sym==SDLK_a)
+    {
+        if (event.type==SDL_KEYDOWN)
+            left = true;
+        else if (event.type==SDL_KEYUP)
+            left = false;
+    }
+    if (event.key.keysym.sym==SDLK_d)
+    {
+        if (event.type==SDL_KEYDOWN)
+            right = true;
+        else if (event.type==SDL_KEYUP)
+            right = false;
+    }
 }
 
 float deltaY = 0;
 
 void FirstPersonController::onMouse(SDL_Event& event)
 {
-	if (event.type != SDL_MOUSEMOTION) return;
-	if (event.motion.xrel < 0)
-		deltaY -= 1;
-	else if (event.motion.xrel > 0)
-		deltaY += 1;
+    if (event.type!=SDL_MOUSEMOTION) return;
+    if (event.motion.xrel<0)
+        deltaY -= 1;
+    else if (event.motion.xrel>0)
+        deltaY += 1;
 
-	rotation = deltaY * 0.5;
+    rotation = deltaY * 0.5;
 }
 
 void FirstPersonController::setInitialPosition(glm::vec2 position, float rotation)
 {
-	this->position = glm::vec3(position.x, 0, position.y);
-	this->rotation = rotation;
+    this->position = glm::vec3(position.x, 0, position.y);
+    this->rotation = rotation;
 }
--- WorldMap.cpp
+++ WorldMap.cpp
@@ -11,47 +11,69 @@
 
 using namespace std;
 
-void WorldMap::loadMap(std::string filename) {
+void WorldMap::loadMap(std::string filename)
+{
     // todo implement json loading this instead following
-    values.push_back( { 0, 0, 0,0} ); // -------> x
-    values.push_back( { 0,-1,-1,2} ); // |
-    values.push_back( { 0,-1,-1,9} ); // |
-    values.push_back( { 0, 0, 0,0} ); // v z
-    startingPosition.x = 1.5;
-    startingPosition.y = 1.5;
-    startingRotation = 22.5;
+    //values.push_back({0, 0, 0,0}); // -------> x
+    //values.push_back({0,-1,-1,2}); // |
+    //values.push_back({0,-1,-1,9}); // |
+    //values.push_back({0, 0, 0,0}); // v z
 
-    // using namespace rapidjson;
-    // ifstream fis(filename);
-    // IStreamWrapper isw(fis);
-    // Document d;
-    // d.ParseStream(isw);
+    using namespace rapidjson;
+    ifstream fis(filename);
+    IStreamWrapper isw(fis);
+    Document d;
+    d.ParseStream(isw);
+
+    const Value& a = d["tileMap"];
+    assert(a.IsArray());
+    for (SizeType i = 0; i<a.Size(); i++)
+    {
+        std::vector<int> row;
+        for (SizeType j = 0; j<a[i].Size(); ++j)
+        {
+            row.push_back(a[i][j].GetInt());
+        }
+        values.push_back(row);
+    }
+
+    startingPosition.x = d["spawn"]["x"].GetFloat();
+    startingPosition.y = d["spawn"]["y"].GetFloat();
+    startingRotation = d["spawn"]["angle"].GetFloat();
+
 }
 
-int WorldMap::getTile(int x, int y) {
+int WorldMap::getTile(int x, int y)
+{
     return values.at(y).at(x);
 }
 
-int WorldMap::getWidth() {
+int WorldMap::getWidth()
+{
     return values[0].size();
 }
 
-int WorldMap::getHeight() {
+int WorldMap::getHeight()
+{
     return values.size();
 }
 
-glm::vec2 WorldMap::getStartingPosition() {
+glm::vec2 WorldMap::getStartingPosition()
+{
     return startingPosition;
 }
 
-float WorldMap::getStartingRotation() {
+float WorldMap::getStartingRotation()
+{
     return startingRotation;
 }
 
-glm::vec4 WorldMap::getFloorColor() {
+glm::vec4 WorldMap::getFloorColor()
+{
     return floorColor;
 }
 
-glm::vec4 WorldMap::getCeilColor() {
+glm::vec4 WorldMap::getCeilColor()
+{
     return ceilColor;
 }
